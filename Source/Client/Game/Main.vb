ec7ee77d569db655747dbc9b6945a668c0d17efc
Seth <Hellator@gmail.com>
2024-11-01 11:10:09 -0500

    chore: Cleanup

--- Source/Client/Game/Main.vb
+++ Source/Client/Game/Main.vb
@@ -1,12 +1,10 @@
-Namespace Modules
-    Module Program
-        Sub Main()
-            ' Start the asynchronous tasks
-            Dim startupTask = Task.Run(Sub() Startup())
-            Dim clientTask = Task.Run(Sub() client.Run())
+Module Program
+    Sub Main()
+        ' Start the asynchronous tasks
+        Dim startupTask = Task.Run(Sub() Startup())
+        Dim clientTask = Task.Run(Sub() client.Run())
 
-            ' Wait for both tasks to complete
-            Task.WaitAll(startupTask, clientTask)
-        End Sub
-    End Module
-End Namespace
+        ' Wait for both tasks to complete
+        Task.WaitAll(startupTask, clientTask)
+    End Sub
+End Module
